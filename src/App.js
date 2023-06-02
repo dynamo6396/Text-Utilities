3c42a0b7ad0acea24e5b2b17b4229965e33602da
dynamo6396 <jatingoyal6396@gmail.com>
2023-06-02 15:08:03 +0530

    adding lower-case-convertor and some more functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 // Everything that you can see in the browser comes after resolving App.js and
 //  when you want to start project from here 
 // let name=" Jatin"
@@ -9,6 +10,9 @@ function App() {
    <>     
    {/* <Navbar title="App" aboutText="aboutusss"/> */}
    <Navbar title="App" />
+   <div className="container">
+    <TextForm heading="Enter the text to anlyze"/>
+   </div>
    </>  
 );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,42 @@
+import React,{useState} from 'react'
+// useState is hook which is used to create text,setText 
+export default function TextForm(props) {
+    const handleUpClick =()=>{
+        // console.log(" Uppercase was clicked "+ text)
+        let newtext=text.toUpperCase();
+        setText(newtext)
+    }
+    const handleLoClick =()=>{
+        // console.log(" Uppercase was clicked "+ text)
+        let newtext=text.toLowerCase();
+        setText(newtext)
+    }
+    const handleOnChange =(event)=>{
+        // console.log("On Change")
+        setText(event.target.value)
+    }
+
+    const [text,setText]=useState('');
+    // text="fasdfa"  wrong way to change text 
+    // setText("newText")  Correct way to change the state
+  return (
+    <>
+        <div>
+            <h1>{props.heading} </h1>
+            <div className="mb-3">
+                {/* <label for="myBox" className="form-label">Example textarea</label> */}
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1 " onClick={handleLoClick}>Convert to LowerCase</button>
+        </div>
+        <div className="container my-3">
+            <h2>Your Text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.08*text.split(" ").length} Minutes to read this content </p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+    </>
+  )
+}
